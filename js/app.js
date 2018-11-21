88382483eb27a24fc1f064c171aa1441d1885ed5
GRUPOAMSA\esanchez <eddysz2000@gmail.com>
2018-11-21 11:44:35 -0500

    creacion de modulo

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
+//creamos el modulo Calculadora para que contenga la logica
+var Calculadora = (function(){
+	return {
+		sumar: function(numero){
+			return numero;
+		}
+	};
+})();
+
+
 //funcion captura de datos del teclado
 function teclado(event){
 	var tecla = event.which || event.keyCode;
-	document.querySelector('.pantalla #display')
-	.innerHTML=String.fromCharCode(tecla);
+	var vtecla ='';
+	if (tecla <81){
+		vtecla = vtecla+String.fromCharCode(tecla);
+		document.querySelector('.pantalla #display').innerHTML=vtecla;
+	}
+	
 }
 //listener para capturar datos del teclado
 document.onkeypress=teclado;
